571775a382ea511ed83c4c80b0958f22ae2a5f85
CodeNinjasDripping <161961323+CodeNinjasDripping@users.noreply.github.com>
2025-02-19 19:21:22 -0600

    Step 42

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
+    [Header("Default speed")]
     // Start is called before the first frame update
+    public float speed;
     void Start()
     {
-
+        transform.position += Vector3.right * speed * Time.deltaTime;
     }
 
     // Update is called once per frame
--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -9,22 +9,33 @@ public class PlayerControls : MonoBehaviour
     public GameController gameController;
     [Header("Velocity")]
     public float velocity = 1;
-    private Rigidbody rb;
+    private Rigidbody2D rb;
     private float objectHeight;
     void Start()
     {
         GameController gc = GetComponent<GameController>();
-        Time.timeScale = 1;
-        Rigidbody2D rb = GetComponent<Rigidbody2D>();
-        float objectHeight = transform.GetComponent<SpriteRenderer>().bounds.size.y / 2;
+        //Time.timeScale = 1;
+         rb = GetComponent<Rigidbody2D>();
+         objectHeight = transform.GetComponent<SpriteRenderer>().bounds.size.y / 2;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             rb.linearVelocity = Vector2.up * velocity;
-        }    
+        }
+
+        
+        
+    }
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "HighSpike" || collision.gameObject.tag == "LowSpike" || collision.gameObject.tag == "Ground")
+        {
+            GameObject.Find("GameController").GetComponent<GameController>().GameOver();
+        }
     }
 }
